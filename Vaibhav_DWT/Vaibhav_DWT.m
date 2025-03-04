6dd9a35f7f9633771b77911900304d83934171db
Vaibhav <90001904+bvsrvaibhav@users.noreply.github.com>
2025-03-04 23:15:29 +0530

    Add files via upload

---
+++ Vaibhav_DWT/Vaibhav_DWT.m
@@ -0,0 +1,40 @@
+clc;
+clearvars;
+close all;
+
+% Read the image
+img = imread('https://w7.pngwing.com/pngs/517/522/png-transparent-language-russia-512x512-logo-emblem-logo-fictional-character.png');
+
+% Convert to grayscale if it's an RGB image
+if size(img, 3) == 3
+    gray_img = rgb2gray(img);
+else
+    gray_img = img;
+end
+
+% Resize the image
+gray_img = imresize(gray_img, [512, 512]);
+
+% Convert to double for DWT
+gray_img = im2double(gray_img);
+
+% Perform 2D Discrete Wavelet Transform using Haar wavelet
+[LL, LH, HL, HH] = dwt2(gray_img, 'haar');
+
+% Display the results
+figure;
+subplot(2,2,1);
+imshow(LL, []);
+title('LL');
+
+subplot(2,2,2);
+imshow(LH, []);
+title('LH');
+
+subplot(2,2,3);
+imshow(HL, []);
+title('HL');
+
+subplot(2,2,4);
+imshow(HH, []);
+title('HH');
